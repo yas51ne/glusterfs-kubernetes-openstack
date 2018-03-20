1116b9bc0099b30670223b2d4dfb78a873ddb76a
yas51ne <yassinemaachi5@gmail.com>
2018-03-20 16:35:46 +0100

    Update post-conf-storage-node.sh

--- post-conf-storage-node.sh
+++ post-conf-storage-node.sh
@@ -11,8 +11,4 @@ systemctl restart systemd-modules-load.service
  
 modprobe dm_thin_pool
 
-echo "10.0.0.10   datalab--0--kube-master-" >> /etc/hosts
-echo "10.0.0.20   datalab--0--kube-minion-" >> /etc/hosts
-echo "10.0.0.21   datalab--1--kube-minion-" >> /etc/hosts
-echo "10.0.0.22   datalab--2--kube-minion-" >> /etc/hosts
-echo "10.0.0.23   datalab--3--kube-minion-" >> /etc/hosts
+./edit-etc-hosts.sh
