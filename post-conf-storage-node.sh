8736093a7fefe1d04f4e3a039c6cae0759055c6f
yas51ne <yassinemaachi5@gmail.com>
2018-03-20 17:46:25 +0100

    Update post-conf-storage-node.sh

--- post-conf-storage-node.sh
+++ post-conf-storage-node.sh
@@ -10,3 +10,6 @@ echo dm_thin_pool >> /etc/modules-load.d/modules.conf
 systemctl restart systemd-modules-load.service
  
 modprobe dm_thin_pool
+
+sed "/ registry.fedoraproject.org/c index.docker.ro \n" /etc/containers/registries.conf
+systemctl restart docker
