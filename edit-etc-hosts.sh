dd31de0dd61a186f90967b410cc5f5d76d326b21
yas51ne <yassinemaachi5@gmail.com>
2018-03-20 16:34:53 +0100

    Create edit-etc-hosts.sh

---
+++ edit-etc-hosts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo -i
+
+echo "10.0.0.10   datalab--0--kube-master-" >> /etc/hosts
+echo "10.0.0.20   datalab--0--kube-minion-" >> /etc/hosts
+echo "10.0.0.21   datalab--1--kube-minion-" >> /etc/hosts
+echo "10.0.0.22   datalab--2--kube-minion-" >> /etc/hosts
+echo "10.0.0.23   datalab--3--kube-minion-" >> /etc/hosts
