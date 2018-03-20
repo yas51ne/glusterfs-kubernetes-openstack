a2518e944d704cf4c3d397d5ed10de8ee0330c96
yas51ne <yassinemaachi5@gmail.com>
2018-03-20 16:37:28 +0100

    Update post-conf-master-node.sh

--- post-conf-master-node.sh
+++ post-conf-master-node.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./edit-etc-hosts.sh
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 
@@ -7,5 +9,4 @@ sudo -i
 
 sed "/registry.fedoraproject.org/c  - index.docker.ro \n" registries.conf
 
-./edit-etc-hosts.sh
 
