ddf47f8f2dbf43da7726de657d98bed1b42f8beb
yas51ne <yassinemaachi5@gmail.com>
2018-03-20 16:35:28 +0100

    Update post-conf-master-node.sh

--- post-conf-master-node.sh
+++ post-conf-master-node.sh
@@ -7,9 +7,5 @@ sudo -i
 
 sed "/registry.fedoraproject.org/c  - index.docker.ro \n" registries.conf
 
-echo "10.0.0.10   datalab--0--kube-master-" >> /etc/hosts
-echo "10.0.0.20   datalab--0--kube-minion-" >> /etc/hosts
-echo "10.0.0.21   datalab--1--kube-minion-" >> /etc/hosts
-echo "10.0.0.22   datalab--2--kube-minion-" >> /etc/hosts
-echo "10.0.0.23   datalab--3--kube-minion-" >> /etc/hosts
+./edit-etc-hosts.sh
 
